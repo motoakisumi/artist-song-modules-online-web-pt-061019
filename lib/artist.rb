6446ba0b024ee54244a237291392c942e9fbb738
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-17 06:07:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,13 +19,15 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #def self.reset_all
+  #  self.all.clear
+  #end
 
-  def self.count
-    self.all.count
-  end
+  #def self.count
+  #  self.all.count
+  #end
+
+  extend Memorable
 
   def add_song(song)
     @songs << song
